d4ec5aad5464916d10058087ceb619217aae516b
BahMartins <bahmartins2@gmail.com>
2022-04-26 15:33:06 -0300

    reading csv files

--- src/main/java/br/com/alura/challend/financialtransactions/controller/ImportTransactionsController.java
+++ src/main/java/br/com/alura/challend/financialtransactions/controller/ImportTransactionsController.java
@@ -1,12 +1,16 @@
 package br.com.alura.challend.financialtransactions.controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -14,20 +18,26 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class ImportTransactionsController {
 
     @GetMapping
-    public String getView(){
-        return "ImportTransactions";
+    public ModelAndView getView(){
+        return new ModelAndView("ImportTransactions.html");
     }
 
     @PostMapping
     public String postTransaction(@RequestParam("file")MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
 
         String fileName = file.getOriginalFilename();
-
         long size = file.getSize();
 
-        System.out.println(fileName + size);
+        System.out.println(fileName + " - " + size);
 
-        return "redirect:ImportTransactions";
+
+        InputStream inputStream = file.getInputStream();
+        new BufferedReader(new InputStreamReader(inputStream))
+                .lines()
+                .forEach(System.out::println);
+
+
+        return "redirect:/importTransactions";
 
     }
 }
